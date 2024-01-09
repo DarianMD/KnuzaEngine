085a6979dcc6033edbcbc477efa56f5fa7bcc970
DarianMD <darianmanasturean@gmail.com>
2024-01-09 17:23:59 +0100

    hello world, create and open windows

---
+++ index.cpp
@@ -0,0 +1,33 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <stdio.h>
+
+int main(int argc, char **argv){
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(1000, 1000, "Knuza Engine", NULL, NULL);
+    
+    if(window == NULL){
+        printf("Error");
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    
+    return 0;
+    
+}
