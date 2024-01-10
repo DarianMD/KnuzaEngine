87c05d29e838c8d92c6c2017a68be021fe6c346a
DarianMD <darianmanasturean@gmail.com>
2024-01-10 18:09:08 +0100

    Render scene, viewport created

--- index.cpp
+++ index.cpp
@@ -3,6 +3,18 @@
 
 #include <stdio.h>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height){
+    glViewport(0,0, width, height);
+}
+
+void renderScene(){
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClearColor(5.0, 5.0, 5.0, 5.0);
+
+
+}
+
 int main(int argc, char **argv){
 
     glfwInit();
@@ -10,24 +22,33 @@ int main(int argc, char **argv){
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(1000, 1000, "Knuza Engine", NULL, NULL);
-    
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Knuza Engine", NULL, NULL);
+
     if(window == NULL){
-        printf("Error");
+        printf("Failed to create GLFW window");
         glfwTerminate();
         return -1;
     }
 
     glfwMakeContextCurrent(window);
 
+
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+        printf("Failed to initialize GLAD");
+    }
+
+    glViewport(0,0,800,600);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
     while (!glfwWindowShouldClose(window))
     {
+        renderScene();
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
     glfwTerminate();
-    
     return 0;
     
 }
